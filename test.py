422373ebe1924d1295ccfc93e85bb091dae251f4
JuanBindez <juanbindez780@gmail.com>
2023-09-22 11:50:05 -0300

    update

--- test.py
+++ test.py
@@ -3,8 +3,13 @@ from pytubefix.cli import on_progress
 
 url = input("URL >")
 
+yt = YouTube(url)
+print(yt.captions)
+
+'''
 yt = YouTube(url, on_progress_callback = on_progress)
+print(yt.title)
 ys = yt.streams.get_highest_resolution()
 ys.download()
-
+'''
 
