11445863d91868283129b7971813e88d0f5e0513
Ebraheem Alhetari <hetari4py@gmail.com>
2024-05-01 19:42:04 +0300

    refactor: some typos

--- pytubefix/colors.py
+++ pytubefix/colors.py
@@ -3,7 +3,7 @@
 
 class Color:
     GREEN = '\033[92m'
-    LIGTH_GREEN = '\033[1;92m'
+    LIGHT_GREEN = '\033[1;92m'
     RED = '\033[91m'
     YELLOW = '\033[93m'
     BLUE = '\033[1;34m'
@@ -11,16 +11,16 @@ class Color:
     BOLD = '\033[;1m'
     CYAN = '\033[1;36m'
     LIGHT_CYAN = '\033[1;96m'
-    LIGTH_GREY = '\033[1;37m'
+    LIGHT_GREY = '\033[1;37m'
     DARK_GREY = '\033[1;90m'
     BLACK = '\033[1;30m'
     WHITE = '\033[1;97m'
-    INVERTE = '\033[;7m'
+    INVERT = '\033[;7m'
     RESET = '\033[0m'
-    
+
 
 GREEN = '\033[92m'
-LIGTH_GREEN = '\033[1;92m'
+LIGHT_GREEN = '\033[1;92m'
 RED = '\033[91m'
 YELLOW = '\033[93m'
 BLUE = '\033[1;34m'
@@ -28,7 +28,7 @@ MAGENTA = '\033[1;35m'
 BOLD = '\033[;1m'
 CYAN = '\033[1;36m'
 LIGHT_CYAN = '\033[1;96m'
-LIGTH_GREY = '\033[1;37m'
+LIGHT_GREY = '\033[1;37m'
 DARK_GREY = '\033[1;90m'
 BLACK = '\033[1;30m'
 WHITE = '\033[1;97m'
