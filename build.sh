7e8da55774e6ff95fabd6db161ee183ba5a0212a
JuanBindez <juanbindez780@gmail.com>
2024-06-28 08:38:38 -0300

    Added ability to get video width and height -> by @CureSaba

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'pytubefix 5.6.3'
-git push -u origin main
-git tag v5.6.3
+git commit -m 'Added ability to get video width and height -> by @CureSaba'
+git push -u origin dev
+git tag v5.7-rc1
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "5.6.3"
+__version__ = "5.7-rc1"
 
 if __name__ == "__main__":
     print(__version__)
