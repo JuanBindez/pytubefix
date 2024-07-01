22fc56d88d72dba312227ca7684e3f35ab34f22c
juan <juanbindez780@gmail.com>
2024-06-30 23:18:07 -0300

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Added ability to get video width and height -> by @CureSaba'
+git commit -m 'update'
 git push -u origin dev
-git tag v5.7-rc1
+git tag v5.7-rc2
 git push --tag
 make clean
 make upload
