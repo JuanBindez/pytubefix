b495e4a923396041f3d854c1125bfb98a720933b
JuanBindez <juanbindez780@gmail.com>
2024-07-10 13:29:36 -0300

    6.0-rc1

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m '5.8.0'
+git commit -m '6.0-rc1'
 git push -u origin dev
-git tag v5.8.0
+git tag v6.0-rc1
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "5.8.0"
+__version__ = "6.0-rc1"
 
 if __name__ == "__main__":
     print(__version__)
