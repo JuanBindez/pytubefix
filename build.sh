02df8fbbf9c5eb1e01689e8c0ea1991f67c88087
juan <juanbindez780@gmail.com>
2024-07-10 23:14:47 -0300

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'pytubefix 6.0.0 ->   Fixed "get_throttling_function_name: could not find match for multiple" and other improvements #94 '
-git push -u origin main
-git tag v6.0.0
+git commit -m 'update'
+git push -u origin dev
+git tag v6.1-rc1
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.0.0"
+__version__ = "6.1-rc1"
 
 if __name__ == "__main__":
     print(__version__)
