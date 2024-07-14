813f1fecb961661ece366979356bc904af00af0c
juan <juanbindez780@gmail.com>
2024-07-14 17:53:48 -0300

    update

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Improved check_availability #106'
+git commit -m 'update'
 git push -u origin dev
-git tag v6.2-rc2
+git tag v6.2-rc3
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.2-rc2"
+__version__ = "6.2-rc3"
 
 if __name__ == "__main__":
     print(__version__)
