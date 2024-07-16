01eda5e1ee3d6a5cdcb8dd709438c1e741b288c2
JuanBindez <juanbindez780@gmail.com>
2024-07-16 08:49:52 -0300

    Fixed issue with use_oauth #111

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'pytubefix 6.2.2 -> b2ac8550da2b73421bde43838b96d7cace979c29 caec2a4f28206de68af082ba3e1e22da3e849879'
-git push -u origin main
-git tag v6.2.2
+git commit -m ' Fixed issue with use_oauth #111 '
+git push -u origin use_oauth_patch
+git tag v6.3-rc1
 git push --tag
 make clean
 make upload
