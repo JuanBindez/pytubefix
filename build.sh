4ca5fa58f04eadeafa77c9730ff3eebf8cf6cb71
JuanBindez <juanbindez780@gmail.com>
2024-07-17 11:06:10 -0300

    merge dev -> use_pathlib

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge branch use_oauth_path -> dev'
-git push -u origin dev
-git tag v6.3-rc2
+git commit -m 'merge dev -> use_pathlib'
+git push -u origin use_pathlib
+git tag v6.3-rc3
 git push --tag
 make clean
 make upload
