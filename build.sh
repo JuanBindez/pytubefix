9bc7c4a49dc62cdd7424dddb739e9485ff8a4ca8
JuanBindez <juanbindez780@gmail.com>
2024-07-17 08:26:38 -0300

    merge branch use_oauth_path -> dev

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m ' Fixed issue with use_oauth #111 '
-git push -u origin use_oauth_patch
-git tag v6.3-rc1
+git commit -m 'merge branch use_oauth_path -> dev'
+git push -u origin dev
+git tag v6.3-rc2
 git push --tag
 make clean
 make upload
