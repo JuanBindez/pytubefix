1a08f597e4475ed0464e2e2fc1ddfd9bfe420a75
JuanBindez <juanbindez780@gmail.com>
2024-07-18 12:41:39 -0300

    merge Error_handlin -> dev 6.3-rc6

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge dev -> Error_handling 6.3-rc5'
-git push -u origin Error_handling
-git tag v6.3-rc5
+git commit -m 'merge Error_handlin -> dev 6.3-rc6'
+git push -u origin dev
+git tag v6.3-rc6
 git push --tag
 make clean
 make upload
