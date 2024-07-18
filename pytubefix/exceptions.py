ba0fc0775c6d8296faf8605f85b4624e70cf9575
zkreutzjans <zkreutzjans+github@gmail.com>
2024-07-18 20:09:14 +0000

    new exception organization

--- pytubefix/exceptions.py
+++ pytubefix/exceptions.py
@@ -1,6 +1,8 @@
 """Library specific exception definitions."""
 from typing import Pattern, Union
 from .colors import Color
+import logging
+logger = logging.getLogger(__name__)
 
 c = Color()
 
@@ -11,6 +13,10 @@ class PytubeFixError(Exception):
     in unintended errors being unexpectedly and incorrectly handled within
     implementers code.
     """
+
+
+### MISC Errors ###
+
 
 class MaxRetriesExceeded(PytubeFixError):
     """Maximum number of retries exceeded."""
@@ -39,8 +45,25 @@ class RegexMatchError(ExtractError):
         self.pattern = pattern
 
 
+### Video Unavailable Errors ###
+# There are really 3 types of errors thrown
+# 1. VideoUnavailable - This is the base error type for all video errors. 
+#   Or a catchall if neither the user or developer cares about the specific error.
+# 2. Known Error Type, Extra info useful for user
+# 3. Unknown Error Type, Important to Developer
+
+
+## 1. VideoUnavailable ##
+
+
 class VideoUnavailable(PytubeFixError):
-    """Base video unavailable error."""
+    """
+    Base video error.
+
+    This is the base error type for all video errors.
+
+    Call this if you can't group the error by known error type and it is not important to the developer.
+    """
     def __init__(self, video_id: str):
         """
         :param str video_id:
@@ -54,63 +77,7 @@ class VideoUnavailable(PytubeFixError):
         return f'{c.RED}{self.video_id} is unavailable{c.RESET}'
 
 
-class AgeRestrictedError(VideoUnavailable):
-    """Video is age restricted, and cannot be accessed without OAuth."""
-    def __init__(self, video_id: str):
-        """
-        :param str video_id:
-            A YouTube video identifier.
-        """
-        self.video_id = video_id
-        super().__init__(self.video_id)
-
-    @property
-    def error_string(self):
-        return f"{c.RED}Video ID = {self.video_id}: is age restricted, and can't be accessed without logging in.{c.RESET}"
-
-
-class AgeCheckRequiredError(VideoUnavailable):
-    def __init__(self, video_id: str):
-        """
-        :param str video_id:
-            A YouTube video identifier.
-        """
-        self.video_id = video_id
-        super().__init__(self.video_id)
-
-    @property
-    def error_string(self):
-        return f"{c.RED}Video ID = {self.video_id}: it has age restrictions and cannot be accessed without confirmation.{c.RESET}"
-
-
-class AgeCheckRequiredAccountError(VideoUnavailable):
-    def __init__(self, video_id: str):
-        """
-        :param str video_id:
-            A YouTube video identifier.
-        """
-        self.video_id = video_id
-        super().__init__(self.video_id)
-
-    @property
-    def error_string(self):
-        return (f"{c.RED}Video ID = {self.video_id}: Sorry, something is wrong. This video may be inappropriate for "
-                f"some users. Sign in to your primary account to confirm your age.{c.RESET}")
-
-
-class LiveStreamError(VideoUnavailable):
-    """Video is a live stream."""
-    def __init__(self, video_id: str):
-        """
-        :param str video_id:
-            A YouTube video identifier.
-        """
-        self.video_id = video_id
-        super().__init__(self.video_id)
-
-    @property
-    def error_string(self):
-        return f'{c.RED}{self.video_id} is streaming live and cannot be loaded{c.RESET}'
+## 2. Known Error Type, Extra info useful for user ##
 
 
 class VideoPrivate(VideoUnavailable):
@@ -125,49 +92,6 @@ class VideoPrivate(VideoUnavailable):
     @property
     def error_string(self):
         return f'{c.RED}{self.video_id} is a private video{c.RESET}'
-
-
-class RecordingUnavailable(VideoUnavailable):
-    def __init__(self, video_id: str):
-        """
-        :param str video_id:
-            A YouTube video identifier.
-        """
-        self.video_id = video_id
-        super().__init__(self.video_id)
-
-    @property
-    def error_string(self):
-        return f'{c.RED}{self.video_id} does not have a live stream recording available{c.RESET}'
-
-class StreamingDataMissing(VideoUnavailable):
-    def __init__(self, video_id: str):
-        """
-        :param str video_id:
-            A YouTube video identifier.
-        """
-        self.video_id = video_id
-        super().__init__(self.video_id)
-    
-    @property
-    def error_string(self):
-        return f'{c.RED}{self.video_id} does not have streaming data{c.RESET}'
-
-class VideoOwnerDeleted(VideoUnavailable):
-    """
-    Video owner has been deleted.
-    """
-    def __init__(self, video_id: str):
-        """
-        :param str video_id:
-            A YouTube video identifier.
-        """
-        self.video_id = video_id
-        super().__init__(self.video_id)
-    
-    @property
-    def error_string(self):
-        return f'{c.RED}{self.video_id} has had its owner deleted{c.RESET}'
 
 
 class MembersOnly(VideoUnavailable):
@@ -202,3 +126,131 @@ class VideoRegionBlocked(VideoUnavailable):
     @property
     def error_string(self):
         return f'{c.RED}{self.video_id} is not available in your region{c.RESET}'
+
+class LoginRequired(VideoUnavailable):
+    def __init__(self, video_id: str):
+        """
+        :param str video_id:
+            A YouTube video identifier.
+        """
+        self.video_id = video_id
+        super().__init__(self.video_id)
+    
+    @property
+    def error_string(self):
+        return f'{c.RED}{self.video_id} requires login to view{c.RESET}'
+
+# legacy livestream error types still supported
+
+class RecordingUnavailable(VideoUnavailable):
+    def __init__(self, video_id: str):
+        """
+        :param str video_id:
+            A YouTube video identifier.
+        """
+        self.video_id = video_id
+        super().__init__(self.video_id)
+
+    @property
+    def error_string(self):
+        return f'{c.RED}{self.video_id} does not have a live stream recording available{c.RESET}'
+
+
+class LiveStreamError(VideoUnavailable):
+    """Video is a live stream."""
+    def __init__(self, video_id: str):
+        """
+        :param str video_id:
+            A YouTube video identifier.
+        """
+        self.video_id = video_id
+        super().__init__(self.video_id)
+
+    @property
+    def error_string(self):
+        return f'{c.RED}{self.video_id} is streaming live and cannot be loaded{c.RESET}'
+
+
+# legacy age restricted error types still supported
+
+class AgeRestrictedError(VideoUnavailable):
+    """Video is age restricted, and cannot be accessed without OAuth."""
+    def __init__(self, video_id: str):
+        """
+        :param str video_id:
+            A YouTube video identifier.
+        """
+        self.video_id = video_id
+        super().__init__(self.video_id)
+
+    @property
+    def error_string(self):
+        return f"{c.RED}{self.video_id} is age restricted, and can't be accessed without logging in.{c.RESET}"
+
+
+class AgeCheckRequiredError(VideoUnavailable):
+    def __init__(self, video_id: str):
+        """
+        :param str video_id:
+            A YouTube video identifier.
+        """
+        self.video_id = video_id
+        super().__init__(self.video_id)
+
+    @property
+    def error_string(self):
+        return f"{c.RED}{self.video_id} has age restrictions and cannot be accessed without confirmation.{c.RESET}"
+
+
+class AgeCheckRequiredAccountError(VideoUnavailable):
+    def __init__(self, video_id: str):
+        """
+        :param str video_id:
+            A YouTube video identifier.
+        """
+        self.video_id = video_id
+        super().__init__(self.video_id)
+
+    @property
+    def error_string(self):
+        return (f"{c.RED}{self.video_id} may be inappropriate for "
+                f"some users. Sign in to your primary account to confirm your age.{c.RESET}")
+
+
+## 3. Unknown Error Type, Important to Developer ##
+
+
+class UnknownVideoError(VideoUnavailable):
+    """Unknown video error."""
+    def __init__(self, video_id: str, status: str=None, reason: str=None, developer_message: str=None):
+        """
+        :param str video_id:
+            A YouTube video identifier.
+        :param str status:
+            The status code of the response.
+        :param str reason:
+            The reason for the error.
+        :param str developer_message:
+            The message from the developer.
+        """
+        self.video_id = video_id
+        self.status = status
+        self.reason = reason
+        self.developer_message = developer_message
+
+        logger.warning(f'Unknown Video Error')
+        logger.warning(f'Video ID: {self.video_id}')
+        logger.warning(f'Status: {self.status}')
+        logger.warning(f'Reason: {self.reason}')
+        logger.warning(f'Developer Message: {self.developer_message}')
+        logger.warning(
+            'Please open an issue at '
+            'https://github.com/JuanBindez/pytubefix/issues '
+            'and provide the above log output.'
+        )
+
+        super().__init__(self.video_id)
+
+    @property
+    def error_string(self):
+        return f'{c.RED}{self.video_id} has an unknown error, check logs for more info{c.RESET}'
