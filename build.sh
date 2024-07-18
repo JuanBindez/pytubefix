06df4d4f0b3d17aed54639771b29850bf7340634
JuanBindez <juanbindez780@gmail.com>
2024-07-18 12:53:44 -0300

    pytubefix 6.3.3 -> #111 #112 #114

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge Error_handlin -> dev 6.3-rc6'
-git push -u origin dev
-git tag v6.3-rc6
+git commit -m 'pytubefix 6.3.3 -> #111 #112 #114'
+git push -u origin main
+git tag v6.3.3
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.3-rc6"
+__version__ = "6.3.3"
 
 if __name__ == "__main__":
     print(__version__)
