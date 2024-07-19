809ace37d61f5e45c08f00cabe265c494bb35345
JuanBindez <juanbindez780@gmail.com>
2024-07-19 09:17:00 -0300

    patch exceptions

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'pytubefix 6.3.3 -> #111 #112 #114'
-git push -u origin main
-git tag v6.3.3
+git commit -m 'patch exceptions'
+git push -u origin exceptions
+git tag v6.3.4-rc1
 git push --tag
 make clean
 make upload
