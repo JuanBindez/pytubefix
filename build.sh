54831c61a8093e289bd8dfb8d6ca9eca6af20bf0
JuanBindez <juanbindez780@gmail.com>
2024-07-20 08:24:01 -0300

    merge OAuth -> cli -> v6.4-rc2

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'tests v6.4-rc1'
+git commit -m 'merge OAuth -> cli -> v6.4-rc2'
 git push -u origin OAuth
-git tag v6.4-rc1
+git tag v6.4-rc2
 git push --tag
 make clean
 make upload
