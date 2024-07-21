4e31f5d50bd6b891b806a1137e1e390bd8474ea1
juan <juanbindez780@gmail.com>
2024-07-21 18:30:55 -0300

    pytubefix 6.5.1 ->  #122 18a4dee8e958d41e05fb7c618d194acc5e75c1cf

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge Alfa -> Release_Candidate v6.5-rc1'
-git push -u origin Release_Candidate
-git tag v6.5-rc1
+git commit -m 'pytubefix 6.5.1 ->  #122 18a4dee8e958d41e05fb7c618d194acc5e75c1cf'
+git push -u origin main
+git tag v6.5.1
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.5-rc1"
+__version__ = "6.5.1"
 
 if __name__ == "__main__":
     print(__version__)
