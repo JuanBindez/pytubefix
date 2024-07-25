6f47045ba4a103c1bb53efe08e0b398fdfcdda73
juan <juanbindez780@gmail.com>
2024-07-24 21:57:02 -0300

    Fix empty chapter list bug #131

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'pytubefix 6.5.1 ->  #122 18a4dee8e958d41e05fb7c618d194acc5e75c1cf'
-git push -u origin main
-git tag v6.5.1
+git commit -m ' Fix empty chapter list bug #131 '
+git push -u origin Release_Candidate
+git tag v6.5.2-rc1
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.5.1"
+__version__ = "6.5.2-rc1"
 
 if __name__ == "__main__":
     print(__version__)
