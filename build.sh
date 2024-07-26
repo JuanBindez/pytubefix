22b4330c3956984340b8f677a4c6c79c0ef57b28
juan <juanbindez780@gmail.com>
2024-07-25 21:44:54 -0300

    merge Alfa -> Release_Candidate

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'update'
-git push -u origin Alfa
-git tag v6.5.3-a2
+git commit -m 'merge Alfa -> Release_Candidate'
+git push -u origin Release_Candidate
+git tag v6.5.3-rc1
 git push --tag
 make clean
 make upload
--- main.py
+++
@@ -1,5 +0,0 @@
-from pytubefix import Channel
-
-
-c = Channel('https://www.youtube.com/channel/UCNJ1Ymd5yFuUPtn21xtRbbw')
-print(c.length)
