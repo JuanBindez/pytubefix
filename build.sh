62f24a235c065e097dd7c0455a877ad1505ada24
juan <juanbindez780@gmail.com>
2024-07-25 21:41:08 -0300

    update

--- build.sh
+++ build.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Refactor video count extraction to handle dynamic JSON structures
-
-- Replaced direct access to 'videosCountText' with recursive search in `find_videos_info` method.
-- Updated `length` property to use the new `find_videos_info` method for extracting video count.
-- Added error handling and default return value 'Unknown' in case of missing data or exceptions.
-'
-git push -u origin channel_length
-git tag v6.5.3-a1
+git commit -m 'update'
+git push -u origin Alfa
+git tag v6.5.3-a2
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.5.3-rc1"
+__version__ = "6.5.3-a2"
 
 if __name__ == "__main__":
     print(__version__)
