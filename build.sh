fb673d0581f5b0b37ef42e9e95dd51bd39caa59a
juan <juanbindez780@gmail.com>
2024-07-27 09:33:51 -0300

    Pytubefix 6.6.3 -> (#137)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge chapters_data_patch1 -> Release_Candidate (#137)'
-git push -u origin Release_Candidate
-git tag v6.6.3-rc1
+git commit -m 'Pytubefix 6.6.3 -> (#137)'
+git push -u origin main
+git tag v6.6.3
 git push --tag
 make clean
 make upload
