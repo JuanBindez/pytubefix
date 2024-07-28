2f2eb52edf2fdae7ceff4ee1fb676065d126d020
juan <juanbindez780@gmail.com>
2024-07-28 10:40:39 -0300

    merge -> Alfa -> Release_Candidate

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge -> playlist_oauth_feature -> Alfa'
-git push -u origin Alfa
-git tag v6.7-a1
+git commit -m 'merge -> Alfa -> Release_Candidate'
+git push -u origin Release_Candidate
+git tag v6.7-rc1
 git push --tag
 make clean
 make upload
