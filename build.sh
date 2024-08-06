913327378dbb8d58a52034b2742246228bec4e56
JuanBindez <juanbindez780@gmail.com>
2024-08-06 10:13:09 -0300

    6.9 RC 1

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m '6.9 Alfa 1'
-git push -u origin dev
-git tag v6.9-a1
+git commit -m '6.9 RC 1'
+git push -u origin Release_Candidate
+git tag v6.9-rc1
 git push --tag
 make clean
 make upload
