ef674b23fc5dd8755b1cc6943f20423db23f12b5
juan <juanbindez780@gmail.com>
2024-08-11 14:18:55 -0300

    fixed and added new functionality #171 #172

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge dev -> Release_Candidate'
-git push -u origin Release_Candidate
-git tag v6.10-rc1
+git commit -m 'fixed and added new functionality #171 #172'
+git push -u origin dev
+git tag v6.10-rc2
 git push --tag
 make clean
 make upload
