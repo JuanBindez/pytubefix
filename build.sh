eea07bd7fb46822671087aac4cf21d935aeac1d4
JuanBindez <juanbindez780@gmail.com>
2024-08-15 10:14:10 -0300

    Pytubefix 6.11.0 (#178)

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Refactor code to ensure compatibility with Python 3.7
-
-- Replaced the 'int | None' type annotation with 'Optional[int]' to support older Python versions.
-- Imported 'Optional' from the 'typing' module.
-'
-git push -u origin dev
-git tag v6.11-rc1
+git commit -m 'Pytubefix 6.11.0 (#178)'
+git push -u origin main
+git tag v6.11.0
 git push --tag
 make clean
 make upload
