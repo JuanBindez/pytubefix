85d07b96842a270cf5af61377d1740e270ab8a86
JuanBindez <juanbindez780@gmail.com>
2024-09-02 16:13:17 -0300

    6.15rc1 Released

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m '6.15a1 Released'
-git push -u origin PoToken
-git tag v6.15-a1
+git commit -m '6.15rc1 Released'
+git push -u origin rc
+git tag v6.15-rc1
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.15-a1"
+__version__ = "6.15-rc1"
 
 if __name__ == "__main__":
     print(__version__)
