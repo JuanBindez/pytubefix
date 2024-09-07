4386652daeb2894d787203e3a4de49d3fcbf8e7a
juan <juanbindez780@gmail.com>
2024-09-07 13:51:10 -0300

    Pytubefix 6.15.2 (#215)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Pytubefix 6.15.1 (Fixed -> SyntaxError: f-string: unmatched '[' #213)'
+git commit -m 'Pytubefix 6.15.2 (#215)'
 git push -u origin main
-git tag v6.15.1
+git tag v6.15.2
 git push --tag
 make clean
 make upload
