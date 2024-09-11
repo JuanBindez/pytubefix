e4a5c1dc3f9ca39143b82045163646904cc2a0ac
JuanBindez <juanbindez780@gmail.com>
2024-09-11 09:40:09 -0300

    merge -> interrupt_checker -> dev

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Pytubefix 6.15.4 (#216 #217)'
-git push -u origin main
-git tag v6.15.4
+git commit -m 'merge -> interrupt_checker -> dev'
+git push -u origin dev
+git tag v6.16-rc1
 git push --tag
 make clean
 make upload
