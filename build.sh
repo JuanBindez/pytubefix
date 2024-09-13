ea9c852dc0751a1d4600aaaa6ac8a0e74971d984
juan <juanbindez780@gmail.com>
2024-09-12 23:14:05 -0300

    Pytubefix 6.16.1 (#218 #223)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'rc2'
-git push -u origin dev
-git tag v6.16-rc2
+git commit -m 'Pytubefix 6.16.1 (#218 #223)'
+git push -u origin main
+git tag v6.16.1
 git push --tag
 make clean
 make upload
