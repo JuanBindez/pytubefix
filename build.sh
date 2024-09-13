bc465c1479165be747780fb618f1823401bf1778
juan <juanbindez780@gmail.com>
2024-09-12 23:06:33 -0300

    rc2

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'merge -> interrupt_checker -> dev'
+git commit -m 'rc2'
 git push -u origin dev
-git tag v6.16-rc1
+git tag v6.16-rc2
 git push --tag
 make clean
 make upload
