a5a71ef7be4398e9e7549c3f8ce06f1e4442e0da
juan <juanbindez780@gmail.com>
2024-09-15 14:04:53 -0300

    Pytubefix 6.16.2 (#228)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Pytubefix 6.16.1 (#218 #223)'
+git commit -m 'Pytubefix 6.16.2 (#228)'
 git push -u origin main
-git tag v6.16.1
+git tag v6.16.2
 git push --tag
 make clean
 make upload
--- pytubefix/version.py
+++ pytubefix/version.py
@@ -1,4 +1,4 @@
-__version__ = "6.16.1"
+__version__ = "6.16.2"
 
 if __name__ == "__main__":
     print(__version__)
