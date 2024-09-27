7287939dabc706d2f37daa2b0710a0921c187f5c
juan <juanbindez780@gmail.com>
2024-09-26 21:46:35 -0300

    7.1-rc2 (#246 #249 #253)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m '7.1-rc1 (#246 #249)'
+git commit -m '7.1-rc2 (#246 #249 #253)'
 git push -u origin dev
-git tag v7.1-rc1
+git tag v7.1-rc2
 git push --tag
 make clean
 make upload
