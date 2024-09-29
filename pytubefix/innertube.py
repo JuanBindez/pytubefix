c55828600f702e9fbbc57b81b0a3638e7f6e2ae8
Jourdelune <64205064+Jourdelune@users.noreply.github.com>
2024-09-29 11:29:37 +0200

    Update innertube.py
    Give data to the api call

--- pytubefix/innertube.py
+++ pytubefix/innertube.py
@@ -496,8 +496,6 @@ class InnerTube:
         self.access_token = None
         self.refresh_token = None
 
-        self.client_name = client
-
         self.access_po_token = None
         self.access_visitorData = None
 
@@ -785,18 +783,23 @@ class InnerTube:
         self.base_data.update({'videoId': video_id, 'contentCheckOk': "true"})
         return self._call_api(endpoint, query, self.base_data)
 
-    def search(self, search_query, continuation=None):
+    def search(self, search_query, continuation=None, data=None):
         """Make a request to the search endpoint.
 
         :param str search_query:
             The query to search.
+        :param str continuation:
+            Continuation token if there is pagination
+        :param dict data:
+            Additional data to send with the request.
         :rtype: dict
         :returns:
             Raw search query results.
         """
         endpoint = f'{self.base_url}/search'
         query = self.base_params
-        data = {}
+        data = data if data else {}
+   
         self.base_data.update({'query': search_query})
         if continuation:
             data['continuation'] = continuation
