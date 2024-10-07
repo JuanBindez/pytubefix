a48195146ea72f7df3959aaa9a6e219efe6a1aca
JuanBindez <juanbindez780@gmail.com>
2024-10-07 16:30:34 -0300

    Pytubefix 7.3.1 (#268)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m 'Pytubefix 7.3.0 (#266)'
+git commit -m 'Pytubefix 7.3.1 (#268)'
 git push -u origin main
-git tag v7.3.0
+git tag v7.3.1
 git push --tag
 make clean
 make upload
