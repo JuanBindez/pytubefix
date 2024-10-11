32a07f244209f2fa78502aa6cfaa05e5bd240211
JuanBindez <juanbindez780@gmail.com>
2024-10-11 12:15:39 -0300

    8.0-rc2 (#277 #278 #279 #280)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m '8.0-rc1 (#277 #278 #279)'
+git commit -m '8.0-rc2 (#277 #278 #279 #280)'
 git push -u origin dev
-git tag v8.0-rc1
+git tag v8.0-rc2
 git push --tag
 make clean
 make upload
