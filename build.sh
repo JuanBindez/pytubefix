1d72384f623df67da85afd5139ee39b7db8976d6
juan <juanbindez780@gmail.com>
2024-10-13 10:32:06 -0300

    8.0.0 (#277 #278 #280 #281 #282)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m '8.0-rc5 (#277 #278 #280 #281 #282)'
-git push -u origin dev
-git tag v8.0-rc5
+git commit -m '8.0.0 (#277 #278 #280 #281 #282)'
+git push -u origin main
+git tag v8.0.0
 git push --tag
 make clean
 make upload
