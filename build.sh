fe2990829053f4603aec9161ec1fd23bb40236ac
juan <juanbindez780@gmail.com>
2024-10-14 21:38:30 -0300

    Pytubefix 8.1.1 (#284)

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git add .
-git commit -m '8.1-rc1 (#284)'
-git push -u origin dev
-git tag v8.1rc1
+git commit -m 'Pytubefix 8.1.1 (#284)'
+git push -u origin main
+git tag v8.1.1
 git push --tag
 make clean
 make upload
