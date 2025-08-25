c39938bdb105cf0769d4a379645eaa09b6a46973
JuanBindez <juanbindez780@gmail.com>
2025-08-25 08:09:23 -0300

    9.5-rc1 (#528 #530 #531)

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -e
 
 VERSION=9
-MINOR=4
-PATCH=1
-EXTRAVERSION=""
-NOTES="(#517 #518 #521)"
+MINOR=5
+PATCH=
+EXTRAVERSION="-rc1"
+NOTES="(#528 #530 #531)"
 BRANCH="main"
 
 if [[ -z $PATCH ]]; then
