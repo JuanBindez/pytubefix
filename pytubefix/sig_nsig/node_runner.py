86c037078a397714d3da4f9726134dbb8c040580
Felipe Ucelli <78266261+felipeucelli@users.noreply.github.com>
2025-09-26 20:12:34 -0400

    Added nodejs-wheel-binaries dependency

--- pytubefix/sig_nsig/node_runner.py
+++ pytubefix/sig_nsig/node_runner.py
@@ -1,20 +1,28 @@
 import os
 import json
 import subprocess
+import nodejs_wheel.executable
+
 
 RUNNER_PATH = os.path.join(os.path.dirname(__file__), "vm", "runner.js")
-
+NODE_DIR = nodejs_wheel.executable.ROOT_DIR
 
 class NodeRunner:
     def __init__(self, code: str):
         self.code = code
         self.function_name = None
         self.proc = subprocess.Popen(
-            ['node', RUNNER_PATH],
+            [self._node_path(), RUNNER_PATH],
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             text=True
         )
+
+    @staticmethod
+    def _node_path() -> str:
+        suffix = ".exe" if os.name == "nt" else ""
+        bin_dir = NODE_DIR if os.name == "nt" else os.path.join(NODE_DIR, "bin")
+        return os.path.join(bin_dir, 'node' + suffix)
 
     @staticmethod
     def _exposed(code: str, fun_name: str) -> str:
